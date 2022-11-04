09e7d0b78f18fa33179e54ec3301be921be0ec59
Sak6680 <117108887+Sak6680@users.noreply.github.com>
2022-11-04 14:16:01 +0600

    Add files via upload

---
+++ time.js
@@ -0,0 +1,23 @@
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    setInterval(function(){
+                var d = new Date();
+            var day = days[d.getDay()];
+            var hr = d.getHours();
+            var min = d.getMinutes();
+            var sec = d.getSeconds()
+            if (min < 10) {
+                min = "0" + min;
+            }
+            var ampm = "am";
+            if( hr > 12 ) {
+                hr -= 12;
+                ampm = "pm";
+            }
+            var date = d.getDate();
+            var month = months[d.getMonth()];
+            var year = d.getFullYear();
+            var x = document.getElementById("time");
+            x.innerHTML = day + " " + hr + ":" + min  + ":" +  sec + " " + ampm + " " + year  + " " + month + " " + date;
+    }, 1000);
